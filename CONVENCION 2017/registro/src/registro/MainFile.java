2dc85db373e12bb1c3b2fe51803db80b5357eda4
wWGonzalez <werner.e.2.g@gmail.com>
2017-09-14 22:29:01 -0600

    Convension2017Luis1

---
+++ CONVENCION 2017/registro/src/registro/MainFile.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package registro;
+
+/**
+ *
+ * @author Luis
+ */
+public class MainFile {
+    public static void main(String[] args) {
+        Functions.theme();
+        //Listado l = new Listado();
+        RegistroEstudiante re = new RegistroEstudiante();
+        //NewJFrame f = new NewJFrame("cat");
+    }
+}
